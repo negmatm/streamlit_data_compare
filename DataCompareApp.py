5b25fb62f1d3071c66df3a4f7d1494643151a399
negmatm <negmat@gmail.com>
2022-04-11 10:03:15 -0400

    Hid data frame index values/column

--- DataCompareApp.py
+++ DataCompareApp.py
@@ -38,10 +38,22 @@ with st.form("my_form"):
 
                 final_list.append([col1_new, col2_new])
 
-                output = pd.DataFrame(final_list, columns = ['Column1', 'Column2'], drop = True)
+                output = pd.DataFrame(final_list, columns = ['Column1', 'Column2'])
 
     # Every form must have a submit button.
     submitted = st.form_submit_button("Show difference")
 
     if submitted:
+        # Hide dataframe index
+        # CSS to inject contained in a string
+        hide_table_row_index = """
+                    <style>
+                    tbody th {display:none}
+                    .blank {display:none}
+                    </style>
+                    """
+
+        # Inject CSS with Markdown
+        st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
         st.table(output)
